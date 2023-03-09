f52e0326ba2acfd66f2ad7a51ba85f2c32da7647
Alexander Li <78469125+bikaneal@users.noreply.github.com>
2023-03-09 17:59:51 +0300

    Update Station.php

--- src/Entity/Station.php
+++ src/Entity/Station.php
@@ -4,15 +4,12 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
-use function PHPSTORM_META\type;
 
 #[ApiResource]
-
 
 #[ORM\Entity]
 class Station 
 { 
-
     #[ORM\Id]
     #[ORM\Column (type: "integer")]
     #[ORM\GeneratedValue (strategy: "AUTO")]
@@ -28,9 +25,4 @@ class Station
     {
         return $this->id;
     }
-
-
-
-
-
 }
